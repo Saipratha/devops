6ba34cada95072bead0af78583b05e8f1b0f80ed
saiprathap <saiprathap.boddula@gmail.com>
2022-09-16 15:17:41 +0530

    inital commit

---
+++ src/main/java/com/project/devops_project/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.project.devops_project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to spring boot app development";
+		
+	}
+	
+}
+
---
+++ src/main/java/com/project/devops_project/devops_project.java
@@ -0,0 +1,15 @@
+package com.project.devops_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class devops_project {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(devops_project.class, args);
+		
+	}
+
+}
